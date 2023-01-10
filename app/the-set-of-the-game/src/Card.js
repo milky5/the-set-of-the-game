d6be756014685a5084c7e72376902932e5b2af58
milky5 <sakurasketch@zoho.com>
2023-01-10 10:15:48 +0900

    shape,colorを描画できる

--- app/the-set-of-the-game/src/Card.js
+++ app/the-set-of-the-game/src/Card.js
@@ -1,19 +1,78 @@
 export function Card(props) {
+  const width = 150;
+  const height = 100;
+  const viewBoxStr = `0 0 ${width} ${height}`;
   return (
-    <button>
-      index: {props.index}
-      <br />
-      ------
-      <br />
-      cardID: {props.id}
-      <br />
-      color: {props.color}
-      <br />
-      shape: {props.shape}
-      <br />
-      paint: {props.paint}
-      <br />
-      number: {props.number}
-    </button>
+    <svg viewBox={viewBoxStr} width={width} height={height}>
+      <rect width={width} height={height} fill="white" stroke="black" />
+      {RenderShape(props.shape, props.color)}
+    </svg>
+  );
+}
+
+function RenderShape(shape, color) {
+  const centerX = 25;
+  const centerY = 50;
+  switch (shape) {
+    case "circle":
+      return Circle(centerX, centerY, color);
+    case "magatama":
+      return Magatama(centerX, centerY, color);
+    case "diamond":
+      return Diamond(centerX, centerY, color);
+    default:
+      return null;
+  }
+}
+
+function Circle(centerX, centerY, color) {
+  const circleX = centerX;
+  const topCircleY = centerY - 25;
+  const bottomCircleY = centerY + 25;
+  const circleRadius = 10;
+  const rectX = centerX - 10;
+  const rectY = centerY - 25;
+  const rectWidth = 20;
+  const rectHeight = 50;
+
+  return (
+    <g>
+      <circle cx={circleX} cy={topCircleY} r={circleRadius} fill={color} />
+      <circle cx={circleX} cy={bottomCircleY} r={circleRadius} fill={color} />
+      <rect
+        x={rectX}
+        y={rectY}
+        width={rectWidth}
+        height={rectHeight}
+        fill={color}
+      />
+    </g>
+  );
+}
+
+function Magatama(centerX, centerY, color) {
+  const leftX = centerX - 10;
+  const rightX = centerX + 10;
+  const topY = centerY - 35;
+  const bottomY = centerY + 35;
+  const pointsStr = `${leftX},${topY} ${rightX},${topY} ${leftX},${bottomY} ${rightX},${bottomY}`;
+
+  return (
+    <g>
+      <polygon points={pointsStr} stroke={color} fill={color} />
+    </g>
+  );
+}
+
+function Diamond(centerX, centerY, color) {
+  const leftX = centerX - 10;
+  const rightX = centerX + 10;
+  const topY = centerY - 35;
+  const bottomY = centerY + 35;
+  const pointsStr = `${centerX},${topY} ${leftX},${centerY} ${centerX},${bottomY} ${rightX},${centerY}`;
+  return (
+    <g>
+      <polygon points={pointsStr} stroke={color} fill={color} />
+    </g>
   );
 }
