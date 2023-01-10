51a96662d9e58fdeb0a5e3c8c1e87081f72e3d48
milky5 <sakurasketch@zoho.com>
2023-01-10 13:06:50 +0900

    塗りつぶし方の分岐ができる(縞模様は仮実装)

--- app/the-set-of-the-game/src/Card.js
+++ app/the-set-of-the-game/src/Card.js
@@ -2,15 +2,23 @@ export function Card(props) {
   const width = 150;
   const height = 100;
   const viewBoxStr = `0 0 ${width} ${height}`;
+  const shapes = RenderShape(
+    props.shape,
+    props.color,
+    props.number,
+    props.paint
+  );
   return (
     <svg viewBox={viewBoxStr} width={width} height={height}>
       <rect width={width} height={height} fill="white" stroke="black" />
-      {RenderShape(props.shape, props.color, props.number)}
+      {shapes.map((element, index) => {
+        return <g key={index}>{element}</g>;
+      })}
     </svg>
   );
 }
 
-function RenderShape(shape, color, count) {
+function RenderShape(shape, color, count, paint) {
   let createComponent;
   switch (shape) {
     case "circle":
@@ -36,16 +44,20 @@ function RenderShape(shape, color, count) {
   const components = [];
   switch (count) {
     case 1:
-      components.push(createComponent(centerCenterX, centerY, color));
+      components.push(createComponent(centerCenterX, centerY, color, paint));
       break;
     case 2:
-      components.push(createComponent(middleLeftCenterX, centerY, color));
-      components.push(createComponent(middleRightCenterX, centerY, color));
+      components.push(
+        createComponent(middleLeftCenterX, centerY, color, paint)
+      );
+      components.push(
+        createComponent(middleRightCenterX, centerY, color, paint)
+      );
       break;
     case 3:
-      components.push(createComponent(leftCenterX, centerY, color));
-      components.push(createComponent(centerCenterX, centerY, color));
-      components.push(createComponent(rightCenterX, centerY, color));
+      components.push(createComponent(leftCenterX, centerY, color, paint));
+      components.push(createComponent(centerCenterX, centerY, color, paint));
+      components.push(createComponent(rightCenterX, centerY, color, paint));
       break;
     default:
       return null;
@@ -54,7 +66,7 @@ function RenderShape(shape, color, count) {
   return components;
 }
 
-function Circle(centerX, centerY, color) {
+function Circle(centerX, centerY, color, paint) {
   const circleX = centerX;
   const topCircleY = centerY - 25;
   const bottomCircleY = centerY + 25;
@@ -63,45 +75,169 @@ function Circle(centerX, centerY, color) {
   const rectY = centerY - 25;
   const rectWidth = 20;
   const rectHeight = 50;
+  const overwriteRectX = rectX + 1;
+  const overwriteRectY = rectY - 1;
+  const overwriteRectWidth = rectWidth - 2;
+  const overwriteRectHeight = rectHeight + 2;
 
-  return (
-    <g>
-      <circle cx={circleX} cy={topCircleY} r={circleRadius} fill={color} />
-      <circle cx={circleX} cy={bottomCircleY} r={circleRadius} fill={color} />
-      <rect
-        x={rectX}
-        y={rectY}
-        width={rectWidth}
-        height={rectHeight}
-        fill={color}
-      />
-    </g>
-  );
+  switch (paint) {
+    case "solid":
+      return (
+        <g>
+          <circle cx={circleX} cy={topCircleY} r={circleRadius} fill={color} />
+          <circle
+            cx={circleX}
+            cy={bottomCircleY}
+            r={circleRadius}
+            fill={color}
+          />
+          <rect
+            x={rectX}
+            y={rectY}
+            width={rectWidth}
+            height={rectHeight}
+            fill={color}
+          />
+        </g>
+      );
+    case "stripes":
+      // TODO: implement
+      return (
+        <g>
+          <circle
+            cx={circleX}
+            cy={topCircleY}
+            r={circleRadius}
+            fill={color}
+            fillOpacity="0.2"
+          />
+          <circle
+            cx={circleX}
+            cy={bottomCircleY}
+            r={circleRadius}
+            fill={color}
+            fillOpacity="0.2"
+          />
+          <rect
+            x={rectX}
+            y={rectY}
+            width={rectWidth}
+            height={rectHeight}
+            fill={color}
+            fillOpacity="0.2"
+          />
+        </g>
+      );
+    case "none":
+      return (
+        <g>
+          <circle
+            cx={circleX}
+            cy={topCircleY}
+            r={circleRadius}
+            fill="transparent"
+            stroke={color}
+          />
+          <circle
+            cx={circleX}
+            cy={bottomCircleY}
+            r={circleRadius}
+            fill="transparent"
+            stroke={color}
+          />
+          <rect
+            x={rectX}
+            y={rectY}
+            width={rectWidth}
+            height={rectHeight}
+            fill="transparent"
+            stroke={color}
+          />
+          <rect
+            x={overwriteRectX}
+            y={overwriteRectY}
+            width={overwriteRectWidth}
+            height={overwriteRectHeight}
+            fill="white"
+            stroke="white"
+          />
+        </g>
+      );
+    default:
+      return null;
+  }
 }
 
-function Magatama(centerX, centerY, color) {
+function Magatama(centerX, centerY, color, paint) {
   const leftX = centerX - 10;
   const rightX = centerX + 10;
   const topY = centerY - 35;
   const bottomY = centerY + 35;
   const pointsStr = `${leftX},${topY} ${rightX},${topY} ${leftX},${bottomY} ${rightX},${bottomY}`;
 
-  return (
-    <g>
-      <polygon points={pointsStr} stroke={color} fill={color} />
-    </g>
-  );
+  switch (paint) {
+    case "solid":
+      return (
+        <g>
+          <polygon points={pointsStr} stroke={color} fill={color} />
+        </g>
+      );
+    case "stripes":
+      // TODO: implement
+      return (
+        <g>
+          <polygon
+            points={pointsStr}
+            stroke={color}
+            fill={color}
+            fillOpacity="0.2"
+          />
+        </g>
+      );
+    case "none":
+      return (
+        <g>
+          <polygon points={pointsStr} stroke={color} fill="transparent" />
+        </g>
+      );
+    default:
+      return null;
+  }
 }
 
-function Diamond(centerX, centerY, color) {
+function Diamond(centerX, centerY, color, paint) {
   const leftX = centerX - 10;
   const rightX = centerX + 10;
   const topY = centerY - 35;
   const bottomY = centerY + 35;
   const pointsStr = `${centerX},${topY} ${leftX},${centerY} ${centerX},${bottomY} ${rightX},${centerY}`;
-  return (
-    <g>
-      <polygon points={pointsStr} stroke={color} fill={color} />
-    </g>
-  );
+
+  switch (paint) {
+    case "solid":
+      return (
+        <g>
+          <polygon points={pointsStr} stroke={color} fill={color} />
+        </g>
+      );
+    case "stripes":
+      // TODO: implement
+      return (
+        <g>
+          <polygon
+            points={pointsStr}
+            stroke={color}
+            fill={color}
+            fillOpacity="0.2"
+          />
+        </g>
+      );
+    case "none":
+      return (
+        <g>
+          <polygon points={pointsStr} stroke={color} fill="transparent" />
+        </g>
+      );
+    default:
+      return null;
+  }
 }
